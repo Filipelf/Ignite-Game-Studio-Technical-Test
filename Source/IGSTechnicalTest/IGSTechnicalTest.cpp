df631ee979cc404e062755114c06c057f88517af
Filipelf <filipefiorentini@gmail.com>
2026-04-20 23:37:06 -0300

    Bootstrap

---
+++ Source/IGSTechnicalTest/IGSTechnicalTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IGSTechnicalTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IGSTechnicalTest, "IGSTechnicalTest" );
